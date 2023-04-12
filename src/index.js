0596bb8baa699f617a062421c4c38e941c4964e1
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-04-12 22:02:41 +0300

    dfcgvnmol,;.'

--- src/index.js
+++ src/index.js
@@ -1,113 +1,65 @@
-import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
+import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import NewsApiServices from './getImage';
 
-const DEBOUNCE_DELAY = 300;
-const refs = {
-  countriesNameInput: document.querySelector('#search-box'),
-  countriesList: document.querySelector('.country-list'),
-  countriesInfo: document.querySelector('.country-info'),
-};
+const form = document.querySelector(".search-form");
+const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector('.load-more');
+const elements = document.querySelectorAll('.photo-card');
 
-refs.countriesNameInput.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+const lightbox = new SimpleLightbox(".gallery a", {});
+const newsApiServices = new NewsApiServices();
+let currentQuantity = newsApiServices.quantity;
 
-function onInputChange(e) {
-  const countryName = e.target.value.trim();
+form.addEventListener("submit", onSearch);
+loadMoreBtn.addEventListener("click", onLoadMoreBtn);
 
-  if (!countryName) {
-    clearTemplate();
-    return;
+function onSearch(e) {
+  e.preventDefault();
+  getQuery(e);
+
+  try {
+    fetchData();
   }
+  catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure("Oops, something went wrong...");
+  }
+}
 
-  fetchCountries(countryName)
-    .then(data => {
-      if (data.length > 10) {
-        specificNameInfo();
-        clearTemplate();
-        return;
-      }
-      renderTemplate(data);
+function createImagesMarkup(images) {
+  return images
+    .map(({ webformatURL, largeImageURL, likes, views, comments, downloads, tags }) => {
+      return `
+        <div class="photo-card">
+          <div class="thumb">
+            <a class="image" href="${webformatURL}">
+            <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+            </a>
+          </div>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${downloads}
+            </p>
+          </div>
+        </div>
+      `;
     })
-    .catch(error => {
-      clearTemplate();
-      errorWarn();
-    });
+    .join("");
+  
 }
 
-function renderTemplate(elements) {
-  let template = '';
-  let refsTemplate = '';
-  clearTemplate();
-
-  if (elements.length === 1) {
-    template = createTemplateItem(elements);
-    refsTemplate = refs.countriesInfo;
-  } else {
-    template = createTemplateItemList(elements);
-    refsTemplate = refs.countriesList;
-  }
-
-  drawTemplate(refsTemplate, template);
-}
-
-function createTemplateItem(element) {
-  return element.map(
-    ({ name, capital, population, flags, languages }) =>
-      `
-      <img
-        src="${flags.svg}" 
-        alt="${name.official}" 
-        width="120" 
-        height="80">
-      <h1 class="country-info__title">${name.official}</h1>
-      <ul class="country-info__list">
-          <li class="country-info__item">
-          <span>Capital:</span>
-        ${capital}
-          </li>
-          <li class="country-info__item">
-          <span>Population:</span>
-          ${population}
-          </li>
-          <li class="country-info__item">
-          <span>Lenguages:</span>
-          ${Object.values(languages)}
-          </li>
-      </ul>
-  `
-  );
-}
-
-function createTemplateItemList(elements) {
-  return elements
-    .map(
-      ({ name, flags }) => `
-      <li class="country-list__item">
-        <img class="country-list__img" 
-          src="${flags.svg}" 
-          alt="${name.official}" 
-          width="60" 
-          height="40">
-        ${name.official}
-      </li>`
-    )
-    .join('');
-}
-
-function specificNameInfo() {
-  Notify.info('Too many matches found. Please enter a more specific name.');
-}
-
-function errorWarn() {
-  Notify.failure(`Oops, there is no country with that name`);
-}
-
-function clearTemplate() {
-  refs.countriesInfo.innerHTML = '';
-  refs.countriesList.innerHTML = '';
-}
-
-function drawTemplate(refs, markup) {
-  refs.innerHTML = markup;
+function addImagesToGallery(markup) {
+  gallery.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
 }
